f72eb2fd530e31f405c80d03ef3f0d04e1bdddff
emerci3 <151554908+emerci3@users.noreply.github.com>
2024-12-10 20:53:54 -0500

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,11 @@
+# Inout display the 2 values
+
+Width = float(input ("Enter the width of the rectangle:"))
+
+Length = float (input ("Enter the length of the rectangle:"))
+
+# Calculate area of the rectangle
+
+Area = (Length * Width)
+
+print ("The calculated area of the rectangle is", Area)
