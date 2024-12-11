a759ba3ae31c234fa795b80ec0eec99023aee964
emerci3 <151554908+emerci3@users.noreply.github.com>
2024-12-10 21:20:08 -0500

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,17 @@
+
+
+
+#Input display the 2 values
+
+Weight = float(input("How much do you weight in Kilograms?:"))
+
+Height = float(input("What is your height is Meters:"))
+
+#Calculate the BMI
+
+BMI= (Weight/(Height**2))
+
+#Display the output
+
+print (" The calculated BMI is",BMI)
+
