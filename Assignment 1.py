6a3ef99650762ad5b5f907a5eb6f181df6f263ed
emerci3 <151554908+emerci3@users.noreply.github.com>
2024-12-10 20:47:22 -0500

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,19 @@
+
+
+
+# Input display the 3 values
+principal = float(input("Enter the principal amount:"))
+
+Rate = float(input("Enter the interest rate in percentage:"))
+
+Time = float(input("Enter the time period in years:"))
+
+# Calculate the simple interest
+
+SimpleInterest= (principal * Rate)/ 100
+
+# Display the output
+
+print ("The caclulated simple interest is", SimpleInterest )
+
+
