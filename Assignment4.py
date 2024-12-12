f49052c3cdc48da8decb855d02ec965128cf9ce9
emerci3 <151554908+emerci3@users.noreply.github.com>
2024-12-11 21:48:42 -0500

    Add files via upload

---
+++ Assignment4.py
@@ -0,0 +1,21 @@
+import math
+
+# Inout display the 2 values
+
+X1= int(input("Enter a number for X1:"))
+
+Y1= int(input("Enter a number for Y1:"))
+
+X2=int(input("Enter a number X2:"))
+
+Y2=int(input ("Enter a number for Y2:"))
+
+# Calculate the distance
+
+Distance= (math.sqrt(X2-X1)**2+((Y2-Y1)**2))
+
+# Display the output
+
+print ("The calculate distance between the two points are", Distance)
+
+
