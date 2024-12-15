38aa50316323c2718ce8bc584909c388647576a8
emerci3 <151554908+emerci3@users.noreply.github.com>
2024-12-14 21:39:44 -0500

    Add files via upload

---
+++ Assignment6.py
@@ -0,0 +1,19 @@
+
+
+#Input Display the values
+
+USD=float(input ("Enter an amount in USD:"))
+
+#Currency conversion
+
+if USD<0:
+
+    print ("Enter a valid currency pair:")
+else:
+
+
+# Output
+
+    GBP= (USD * 0.75)
+
+    print ("The converted value from USD to GBP is:", GBP)
